ea51a2316a855d8da003864f11554c9c7aa3bea7
penghouse <penghouse@gmail.com>
2023-12-02 16:37:03 +0900

    Completed app router tutorial 5

---
+++ apps/nextjs-dashboard/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { LayoutProps } from '@/.next/types/app/dashboard/layout';
+import { ReactElement } from 'react';
+import SideNav from '../ui/dashboard/sidenav';
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
